7fadb060545e2f204889f37fd8ba8763b674b2a2
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-02-04 22:18:16 -0300

    test1

--- api/auth/mensaje.js
+++ api/auth/mensaje.js
@@ -2,7 +2,11 @@
 export default function handler(req, res) {
     if (req.method === "GET") {
       res.status(200).json({ mensaje: "Hola" });
-    } else {
+    } 
+    else if  (req.method === "POST"){
+      res.status(200).json({ mensaje: req.data });
+    }
+    else {
       res.status(405).json({ error: "Método no permitido" });
     }
   }
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [user, setUser] = useState("");
+  const handlerSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log(email, user);
+
+    try {
+      const response = await fetch("/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, user }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+
+      const data = await response.json(); // Convertir la respuesta a JSON
+      console.log("Respuesta del servidor:", data);
+    } catch (error) {
+      console.error("Error en la petición:", error);
+    }
+  };
   return (
     <>
       <h1>Hola, Bienvenidos A Peliculas App Web!</h1>
+      <form onSubmit={handlerSubmit}>
+        <input type="text" onChange={(e) => setEmail(e.target.value)} />
+        <input type="text" onChange={(e) => setUser(e.target.value)} />
+        <input type="submit" />
+      </form>
     </>
   );
 }

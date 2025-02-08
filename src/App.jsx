cb8aa7aede0ed0e7c630994a048ee9391fdcdb13
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-02-08 13:27:29 -0300

    agree react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { LoginForm } from "./components/LoginForm";
 import { RegisterForm } from "./components/RegisterForm";
 
 function App() {
   return (
-    <>
-      <h1>Hola, Bienvenidos A Peliculas App Web!</h1>
-      <LoginForm />
-      <RegisterForm />
-    </>
+    <Router>
+      <Routes>
+        {/* Ruta para la página principal */}
+        <Route
+          path="/"
+          element={
+            <>
+              <h1>Hello Welocome To App Movies</h1>
+            </>
+          }
+        />
+
+        {/* Ruta para el login */}
+        <Route path="/login" element={<LoginForm />} />
+
+        {/* Ruta para los favoritos */}
+        <Route path="/favorites" element={<RegisterForm />} />
+
+        {/* Ruta por defecto para cuando no hay coincidencia */}
+        <Route path="*" element={<div>404 - Página no encontrada</div>} />
+      </Routes>
+    </Router>
   );
 }
 

9b2f19106dbb25c4c7242a3c95737f329e01ea45
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-02-10 11:42:29 -0300

    fix detalle.jsx component test

--- src/components/Detalle.jsx
+++ src/components/Detalle.jsx
@@ -7,7 +7,7 @@ export default function Detalle({ token, logout }) {
   const [movie, setMovie] = useState(null);
   const navigate = useNavigate();
   const { movieID } = useParams(); // 🔹 Obtiene el ID desde la URL
-  console.log(movieID);
+
   useEffect(() => {
     if (!token) {
       navigate("/");
@@ -33,8 +33,10 @@ export default function Detalle({ token, logout }) {
 
     if (movieID) {
       fetchMovieDetail();
+      console.log(movieID);
+      console.log(movie);
     }
-  }, [movieID]);
+  }, [movieID, movie]);
 
   return (
     <div>

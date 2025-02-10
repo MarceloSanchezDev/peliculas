fab3093b4138834bbda7a46478103b8cf2f9e149
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-02-10 15:13:56 -0300

    connect Bsucador Component with Resultado Component

--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -1,10 +1,23 @@
 import { useState } from "react";
-
+import swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
 export default function Buscador() {
+  const navigate = useNavigate();
   const [buscado, setBuscado] = useState("");
   const handlerSearch = (e) => {
     e.preventDefault();
     console.log("buscandooo...", buscado);
+    if (buscado.length < 4) {
+      swal.fire({
+        title: "Error!",
+        text: "Debes escribir una palabra clave",
+        icon: "error",
+        confirmButtonText: "Ok",
+      });
+    } else {
+      e.currentTarget.keyword.value = "";
+      navigate(`/resultados?keyword=${buscado}`);
+    }
   };
   return (
     <form onSubmit={handlerSearch}>

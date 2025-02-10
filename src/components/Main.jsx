5540d3044b8f8d754a9e17a4da7be1107fbacf8e
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-02-09 22:32:01 -0300

    test navigate

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
 import { useNavigate } from "react-router";
 export function Main() {
   const navigate = useNavigate();
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    console.log(token);
+    if (token) {
+      navigate("/Listado");
+    }
+  }, []);
   return (
     <>
       <h1>Hello, Welcome to App Movie</h1>

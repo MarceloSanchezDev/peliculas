993bd6f7290d0eaf58207bf4357a7fb9c533f721
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-02-18 11:18:08 -0300

    fix link nav component

--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -86,9 +86,9 @@ export const Listado = ({ token, logout }) => {
     <div className="d-flex flex-column justify-content-center align-items-center">
       <nav className="navbar navbar-expand-lg navbar-light bg-light w-100">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
+          <Link className="navbar-brand" href="#">
             App Movie
-          </a>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"

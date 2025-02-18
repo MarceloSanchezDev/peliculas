3720eedcb1541086a77b57f89a333976fb4e215a
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-02-18 14:18:12 -0300

    fix nav duplicate

--- src/components/Detalle.jsx
+++ src/components/Detalle.jsx
@@ -39,38 +39,6 @@ export default function Detalle({ token }) {
 
   return (
     <div className="d-flex flex-column justify-content-center align-items-center">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light w-100">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to={"/"}>
-            App Movie
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarContent"
-            aria-controls="navbarContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarContent">
-            <div className="d-flex flex-grow-1 justify-content-between align-items-center mt-2 mt-lg-0">
-              <div className="d-flex ms-auto align-items-center gap-2">
-                <Buscador />
-                <button
-                  className="btn btn-outline-danger text-nowrap"
-                  onClick={logout}
-                >
-                  Log Out
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </nav>
       <h1>Informacion de la Pelicula</h1>
       {movie ? (
         <div className="container mt-4">

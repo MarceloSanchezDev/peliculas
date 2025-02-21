5a4388680c0d8720de2c78124f06c588c2d89013
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-02-21 10:20:16 -0300

    fix d-flex class

--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -111,7 +111,7 @@ export const Listado = ({ token }) => {
               </p>
               <Link
                 to={`/detalle?movieID=${pelicula.id}`}
-                className="btn btn-primary mt-2 dflex justify-content-center"
+                className="btn btn-primary  d-flex justify-content-center"
               >
                 View Detail
               </Link>
